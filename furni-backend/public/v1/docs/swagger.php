80776668a1f0b2e874a9ea000b666eb559172da3
Emina <emina.omercevic@stu.ibu.edu.ba>
2024-06-04 01:51:37 +0200

    deploymentChanges

--- furni-backend/public/v1/docs/swagger.php
+++ furni-backend/public/v1/docs/swagger.php
@@ -2,8 +2,12 @@
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
-define('BASE_URL', 'http://localhost:80/WPEmina/furni-backend/');
-
+//define('BASE_URL', 'http://localhost:80/WPEmina/furni-backend/');
+if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
+    define('BASE_URL', 'http://localhost:80/WPEmina/furni-backend/');
+} else {
+    define('BASE_URL', 'https://coral-app-mx3to.ondigitalocean.app/furni-backend/');
+}
 error_reporting(0);
 
 $openapi = \OpenApi\Generator::scan(['../../../rest/routes', './']);
